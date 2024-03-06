358d73eacae8e7c22eeaa9456fd6d83eb50d3f6c
Eveline Fotso <Sf656145023pgmail.com>
2024-03-06 12:21:56 +0100

    secondMaintenance

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react'
-import {nodeResolve} from '@rollup/plugin-node-resolve'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(),nodeResolve()],
+  plugins: [reactRefresh()],
   build: {
     rollupOptions: {
-      external: ['bootstrap/dist/js/bootstrap.bundle.min'],
+      external: ['react-router-dom'],
     },
   },
 });
